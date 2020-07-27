412486ef208295fccf4200b2fa79c3899898fe58
Adekoya Adebayo <adekoya.adebayojubril@gmail.com>
2020-07-27 15:22:07 +0100

    Update session.php

--- online/session.php
+++ online/session.php
@@ -2,6 +2,6 @@
  session_start(); 
  require('config.php');
  $user_check = $_SESSION['login_user']; 
- $ses_sql = mysqli_query($conn,"SELECT user FROM admin WHERE user = '$user_check'"); 
+ $ses_sql = mysqli_query($conn,"SELECT * FROM admin WHERE user = '$user_check'"); 
  $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC); 
  $login_session = $row['id']; if(!isset($_SESSION['login_user'])){ header("location:login.php"); } ?>
